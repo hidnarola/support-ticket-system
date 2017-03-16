a580e50aaf3648677d2d5ff11ee79d22b7ecb50b
NAROLA\vparth <pav@narola.email>
2017-03-16 15:02:27 +0530

    no message

--- application/config/routes.php
+++ application/config/routes.php
@@ -51,7 +51,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
   |		my-controller/my-method	-> my_controller/my_method
  */
 //$route['default_controller'] = 'Home';
-  $route['default_controller'] = 'Properties';
+  $route['default_controller'] = 'Properties/home2';
 $route['404_override'] = 'Login/page_not_found';
 $route['admin/access_denied'] = 'Admin/Dashboard/access_denied';
 $route['translate_uri_dashes'] = FALSE;
@@ -161,7 +161,8 @@ $route['signup'] = "Login/signup";
 $route['login'] = "Login";
 
 $route['profile'] = "Profile";
-$route['home'] = "Home";
+//$route['home'] = "Home";
+$route['home'] = 'Properties';
 $route['home2'] = "Properties/home2";
 $route['forgot_password'] = "Home/forgot_password";
 $route['reset_password'] = "Home/reset_password";
