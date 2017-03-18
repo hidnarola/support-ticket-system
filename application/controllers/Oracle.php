66475e0e172f9dec46959d74a71d62ac9cdff976
Shamsabbas Gheewala <gshamsabbas@narola.local>
2017-03-18 11:17:39 +0530

    no message

--- application/controllers/Oracle.php
+++ application/controllers/Oracle.php
@@ -22,7 +22,7 @@ class Oracle extends CI_Controller {
         $oracle_url = '83.111.41.30';
         $oracle_port = '1536';
         $db= "(DESCRIPTION=(ADDRESS=(PROTOCOL=tcp)(HOST=83.111.41.30)(PORT=1536))(CONNECT_DATA=(SID=TEST)))";
-        $connect = @oci_connect($oracle_user, $oracle_pass, $db, 'UTF8');
+        $connect = oci_connect($oracle_user, $oracle_pass, $db, 'UTF8');
         echo $connect; exit;
         if (!$connect) {
             die("Could not connect to the database");
