62a915a4fb3f9ec0f33442a3d923c0f0c11d868c
NAROLA\vparth <pav@narola.email>
2017-03-22 16:28:14 +0530

    no message

--- application/views/Templates/propertyfinder/frontend/page.php
+++ application/views/Templates/propertyfinder/frontend/page.php
@@ -2,8 +2,6 @@
    $page = $this->uri->segment(1);
    $current_page = $this->uri->segment(2);
    $page_url = $this->uri->segment(3);
-   $pass = 'tabrabbit1234567tabrabbit';
-   echo hash('sha256',$pass); die;
 ?>
 <!DOCTYPE html>
 <html lang="en-US" class="scheme_original">
