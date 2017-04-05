6fe0c0d9b76b45222bcf4552de54ae03c4d44805
NAROLA\vparth <pav@narola.email>
2017-04-05 18:10:49 +0530

    no message

--- application/views/Templates/propertyfinder/home.php
+++ application/views/Templates/propertyfinder/home.php
@@ -123,6 +123,10 @@
                                  <li class="menu-menu <?php if($page=='portfolio'){ echo 'current-menu-parent'; } ?>">
                                     <a href="portfolio">Portfolio</a>
                                  </li>
+                                 
+                                 <li class="menu-menu">
+                                    <a href="http://www.virtualdusk.net/MANAZEL/al-reef-2-villa-mockup/">Gallery</a>
+                                 </li>
                                  <!-- <li class="menu-item <?php if($current_page=='pages' && $page_url=='about-us'){ echo 'current-menu-parent'; } ?>"><a href="property-finder/pages/about-us">About Us</a></li>
                                  <li class="menu-item"><a href="#">Service</a></li>
                                  <li class="menu-item"><a href="#">Media</a></li>
