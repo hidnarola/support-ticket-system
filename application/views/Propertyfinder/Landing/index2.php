d680c5a1150d0d6acdae5c9c617b49c64985ee19
NAROLA\vparth <pav@narola.email>
2017-05-05 16:36:42 +0530

    no message

--- application/views/Propertyfinder/Landing/index2.php
+++ application/views/Propertyfinder/Landing/index2.php
@@ -1,8 +1,8 @@
 <link href="http://www.jqueryscript.net/css/jquerysctipttop.css" rel="stylesheet" type="text/css">
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-<div class="row about_us_row">
+<div class="row about_us_row" style="min-height: 700px;">
    
-   <div class="col-md-12 pull-right">
+   <div class="col-md-12">
      <?php if(count($landing_banner)>0){ ?>
          <section class="slider_wrap slider_fullwide slider_engine_revo slider_alias_revsliderHome1">
             <!-- REVOLUTION SLIDER -->
